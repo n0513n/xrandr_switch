30cdddaa2ec9c3bbacacc084c58c8eee56217b99
n0513n <n3150n@outlook.com>
2020-04-21 13:35:39 -0300

    code tweaked, fixed clone option and finished custom res (--force)

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 xrandr_switch
 ---
 
-Simple bash script to interact with ```xrandr``` and set display and screen solutions.
+Simple bash script to interact with `xrandr` and set display and screen solutions.
+
+Allows `--force` parameter to add and set a custom resolution (requires `cvt`).
 
 ```
 usage: xswitch {option} [mode] [--force]
@@ -11,10 +13,28 @@ options:
   --internal  internal computer screen only
   --external  external screen only (HDMI)
   --dual      dual head (twin view)
-  --clone     mirror both screens 
+  --clone     mirror both screens
   --off       disable screens (!)
 ```
 
+### Examples
+
+Turn on all screen monitors on default resolution:
+
+* `xswitch --auto`
+
+Force custom 1152x648 resolution on internal screen monitor:
+
+* `xswitch internal 1152x648 --force`
+
+Twin view screen monitors using 1920x1080 external resolution:
+
+* `xswitch dual 1920x1080`
+
+Mirror both screen monitors using the same 1366x768 resolution:
+
+* `xswitch clone 1366x768`
+
 ### Current limitations
 
-* Since this depends on ```xrandr``` (X/RandR), no Wayland for now.
+Since this depends on `xrandr` (X/RandR), Wayland is unsupported.
--- xswitch.sh
+++ xswitch.sh
@@ -9,32 +9,26 @@
 #   --internal  internal computer screen only
 #   --external  external screen only (HDMI)
 #   --dual      dual head (twin view)
-#   --clone     mirror both screens 
+#   --clone     mirror both screens
 #   --off       disable screens (!)
 
-# define user settings #
-
+# define user settings
 NAME_LEFT="$(xrandr | grep connected | grep -v VIRTUAL | sed 's/ .*//' | head -n 1)"
 NAME_RIGHT="$(xrandr | grep connected | grep -v VIRTUAL | sed 's/ .*//' | tail -n 1)"
 
-# define vars #
-
+# define vars
 LEFT="$(xrandr | grep $NAME_LEFT | awk '{print $1}' | tail -n 1)"
 RIGHT="$(xrandr | grep $NAME_RIGHT | awk '{print $1}' | tail -n 1)"
 
 ARG="$(echo "$1" | sed s:-::g)" # argument to execute
-VAR="${2,,}" # set screen monitor custom resolution
-OPT="${3,,}" # set optional arg or other screen res
+VAR="${2,,}" # set screen monitor resolution to display
 
 NUM_SCREENS="$(xrandr | grep -c connected -w)"
 
-[[ "$VAR" = "-f" || "$OPT" = "-f" ]] &&
+[[ "$@" = *--force* ]] &&
 force=true
 
-[[ "$VAR" = "-f" ]] &&
-VAR="$OPT"
-
-# define functions #
+# define functions
 
 function help {
     head -n 13 "$0" | tail -n 8 | sed 's/# //'; }
@@ -51,60 +45,60 @@ function set_off {
     xrandr --output $LEFT --off --output $RIGHT --off; }
 
 function set_res {
-    if [[ "$VAR" = off && "$OPT" = off ]]; then
-        # turn both monitors off
-        set_off
-    elif [[ "$VAR" = off ]]; then
-        # turn this monitor off
-        xrandr --output $OUTPUT --off
-    elif [[ "$OPT" = off ]]; then
-        # turn other monitor off
-        xrandr --output $OUTPUT --mode "$VAR" --output "$ALTPUT" --off
+    if [[ "$VAR" = off ]]; then
+        # turn first monitor off
+        xrandr --output "$OUTPUT" --off
     elif [[ "$VAR" = "" || "$VAR" = on || "$VAR" = auto ]]; then
         # change to automatic resolution
-        xrandr --output $OUTPUT --auto
+        xrandr --output "$OUTPUT" --auto
     elif [[ "$VAR" != "" ]]; then
         # change to custom resolution
-        xrandr --output $OUTPUT --mode "$VAR"; fi; }
+        xrandr --output "$OUTPUT" --mode "$VAR"; fi; }
 
 function set_dual {
     # change screen positions
     if [[ "$VAR" != "" ]]; then
-        xrandr --auto --output "$RIGHT" --mode "$VAR" --right-of "$LEFT" --mode "$VAR"
+        xrandr --output "$RIGHT" --mode "$VAR" --right-of "$LEFT" --mode "$VAR"
     else # set automatically
-        xrandr --auto --output "$RIGHT" --auto --right-of "$LEFT"; fi; }
+        xrandr --output "$RIGHT" --auto --right-of "$LEFT"; fi; }
 
-function force_res {
-    # add custom resolution to xrandr available modes
-    # examples taken from "cvrt"
-    if [[ "$VAR" = "1920x1080_60.00" ]]; then
-        xrandr --newmode "1920x1080_60.00" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync &&
-        xrandr --addmode $OUTPUT "1920x1080_60.00" && echo "Mode 1920x1080_60.00 added."
-    elif [[ "$VAR" = "1600x900_60.00" ]]; then
-        xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync &&
-        xrandr --addmode $OUTPUT "1600x900_60.00" && echo "Mode 1600x900_60.00 added."
-    elif [[ "$VAR" = "1280x720_60.00" ]]; then
-        xrandr --newmode "1280x720_60.00"  74.50  1280 1344 1472 1664  720 723 728 748 -hsync +vsync &&
-        xrandr --addmode $OUTPUT "1280x720_60.00" && echo "Mode 1280x720_60.00 added."
-    elif [[ "$VAR" = "1024x768_60.00" ]]; then
-        xrandr --newmode "1024x768_60.00"   63.50  1024 1072 1176 1328  768 771 775 798 -hsync +vsync &&
-        xrandr --addmode $OUTPUT "1024x768_60.00" && echo "Mode 1024x768_60.00 added."
-    elif [[ "$VAR" = "800x600_60.00" ]]; then
-        xrandr --newmode "800x600_60.00"   38.25  800 832 912 1024  600 603 607 624 -hsync +vsync &&
-        xrandr --addmode $OUTPUT "800x600_60.00" && echo "Mode 800x600_60.00 added."; fi; }
+function set_clone {
+    # change screen positions
+    if [[ "$VAR" != "" ]]; then
+        xrandr --output "$RIGHT" --mode "$VAR" --output "$LEFT" --scale-from "$VAR"
+    else # set automatically
+        xrandr --output "$RIGHT" --auto --output "$LEFT" --auto; fi; }
 
-# execute #
+function force_add {
+    # add custom mode to screen (requires cvt)
+    XRES="$(echo $VAR | cut -f1 -d'x')"
+    YRES="$(echo $VAR | cut -f2 -d'x')"
+    REFR="$(echo $VAR | cut -f3 -d'x')"
+    [[ "$REFR" = "" ]] && REFR=60
+    echo "Setting $OUTPUT to ${XRES}x${YRES}x${REFR}..."
+    MODE="$(cvt $XRES $YRES $REFR | tail -n 1 | sed 's/Modeline //' | tr -d \")"
+    VAR="$(echo $MODE | cut -f1 -d' ')"
+    xrandr --rmmode $customres 2>/dev/null
+    xrandr --delmode $OUTPUT $VAR 2>/dev/null
+    (xrandr --newmode $MODE) 2>/dev/null
+    (xrandr --addmode $OUTPUT $VAR) 2>/dev/null; }
+
+# execute
 
 case "$ARG" in
 
     internal)
-        [[ $force = true ]] && force_res
-        OUTPUT="$LEFT"; ALTPUT="$RIGHT"; set_res
+        OUTPUT="$LEFT"
+        ALTPUT="$RIGHT"
+        [[ $force = true ]] && force_add
+        set_res
         ;;
 
     external)
-        [[ $force = true ]] && force_res
-        OUTPUT="$RIGHT"; ALTPUT="$LEFT"; set_res
+        OUTPUT="$RIGHT"
+        ALTPUT="$LEFT"
+        [[ $force = true ]] && force_add
+        set_res
         ;;
 
     dual)
